10cc1fd098f10547b0b0eee7c97924414656840f
Letícia <lgs12@discente.ifpe.edu.br>
2026-08-28 21:26:53 -0300

    remove button (correction)

--- script.js
+++ script.js
@@ -10,24 +10,21 @@ function adicionarPessoa(){
     input.value = "";
 
     exibirLista(lista);
-
-    return lista;
 }
 
-function removerPessoa(id){
-    const index = pessoas.indexOf(id)
-
-    pessoas.splice(index, 1);
+function removerPessoa(cont){
+    pessoas.splice(cont, 1);
 
     console.log(pessoas);
 
     exibirLista(lista);
 }
 
-
 function exibirLista(lista){
+    let cont = 0;
     lista.innerHTML = "";
     for (let nome of pessoas){
-            lista.innerHTML += `<li class="nomes pl-4 flex flex-row justify-between">${nome} <button id="${nome}" onclick="removerPessoa(id)"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17></line></svg></button></li>`;
+            lista.innerHTML += `<li class="nomes pl-4 flex flex-row justify-between">${nome} <button id="${cont}" onclick="removerPessoa(id)"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17></line></svg></button></li>`;
+            cont += 1;
     }
 }

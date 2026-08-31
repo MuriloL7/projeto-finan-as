c3a732de9140007e96b081110babc39cc74ffc7a
MuriloL7 <murilolimadesousa@gmail.com>
2026-08-30 21:57:32 -0300

    create navbar

--- Script/script.js
+++ Script/script.js
@@ -48,7 +48,7 @@ function exibirLista(){
     let cont = 0;
     lista.innerHTML = "";
     for (let nome of pessoas){
-            lista.innerHTML += `<li class="nomes pl-4 flex flex-row justify-between">${nome} <button id="${cont}" onclick="removerPessoa(id)"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17></line></svg></button></li>`;
+            lista.innerHTML += `<li class="border border-slate-400 focus:border-slate-400 rounded-md nomes pl-4 flex flex-row justify-between">${nome} <button id="${cont}" onclick="removerPessoa(id)"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"></path><path d="m6 6 12 12"></path></svg></button></li>`;
             cont += 1;
     }
 }
